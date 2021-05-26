fdd5fab7b3699628b3dec6e287339f5ba993c348
Marie-Carleen Frei <lene@7freiheiten.de>
2021-05-26 12:22:50 +0200

    Implement readline for mutliple answers

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const rl = readline.createInterface({
 rl.question(
   'Which functions and files would you like to create? Plase seperate by comma.',
   answer => {
-    // 'answer' is a string with comma seperated words
-    // for each word we want to execute writeFile
-    //writeFile(answer)
+    answer
+      .split(',')
+      .map(name => name.trim())
+      .forEach(answer => writeFile(answer))
 
-    console.log(answer)
     rl.close()
   }
 )
