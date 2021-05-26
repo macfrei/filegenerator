33b86fa893446ffedae59dc6a4c5bf831257b8b2
Marie-Carleen Frei <lene@7freiheiten.de>
2021-05-26 14:21:09 +0200

    Add checkbox question

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ const questions = [
     message:
       'Which functions and files would you like to create? Plase seperate by comma.',
   },
+  {
+    type: 'checkbox',
+    message: 'Plase select a file type',
+    name: 'fileTypes',
+    choices: [{ name: 'Component' }, { name: 'Spec' }, { name: 'Stories' }],
+    validate(answer) {
+      if (answer.length < 1) {
+        return 'You must choose at least one file type.'
+      }
+      return true
+    },
+  },
 ]
 
 inquirer.prompt(questions).then(answers => {
   const files = answers['file'].split(',').map(el => el.trim())
   files.forEach(file => writeFile(file))
-  console.log(files)
+  console.log(answers)
 })
