ea36e07ab4316fbb34c6c4cba2d482ce482c4702
Marie-Carleen Frei <lene@7freiheiten.de>
2021-05-26 15:33:14 +0200

    Add templates object and rewrite callback

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const writeFile = require('./writeFile')
 const inquirer = require('inquirer')
 
+const templates = {
+  component: name =>
+    `
+  import styled from 'styled-components/macro'
+    
+  export default function ${name} () {
+      return <div>${name}</div>
+  }
+        `,
+  spec: name =>
+    `
+  import {render, screen} from '@testing-library/react'
+  import ${name} from './${name}'
+    
+  describe('${name}', () => {
+      it('renders', () => {
+          render(<${name} />)
+          expect(screen.getByText('${name}')).toBeInTheDocument()
+      })
+  })
+        `,
+  stories: name =>
+    `
+  import ${name} from './${name}'
+  export default {
+      title: '${name}',
+      component: ${name}
+  }
+    
+  const Template = args => <${name} {...args} />
+    
+  export const Default = Template.bind({})
+  Default.args = {}
+        `,
+}
+
 const questions = [
   {
     type: 'input',
@@ -23,7 +59,14 @@ const questions = [
 ]
 
 inquirer.prompt(questions).then(answers => {
-  const names = answers['name'].split(',').map(el => el.trim())
-  names.forEach(name => writeFile(name))
-  console.log(answers)
+  // answers = { name: 'Header', fileTypes: ['component', 'spec']}
+
+  // iterate over fileTypes array from answer
+  answers.fileTypes.forEach(fileType => {
+    // get value (our template function) from each fileType
+    const templateFunction = templates[fileType]
+    // call function and save string
+    const fileString = templateFunction(answers.name)
+    writeFile(answers.name, fileType, fileString)
+  })
 })
